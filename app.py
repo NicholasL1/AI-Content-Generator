88272b9f53142422e6a3ec9a11e7810c40bb0e49
Nicholas Lachhman <nickla@my.yorku.ca>
2024-06-21 14:34:20 -0400

    Updated app for new openai model

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ cohere_api_key = os.environ.get('cohere_api_key')
 qdrant_url = os.environ.get('qdrant_url')
 qdrant_api_key = os.environ.get('qdrant_api_key')
 
-#Flask config
+# Flask config
 app = Flask(__name__)
 CORS(app)
 
 # Test default route
 @app.route('/')
 def hello_world():
-    return {"Hello":"World"}
+    return {"Hello": "World"}
 
-## Embedding code
+# Embedding code
 from langchain.embeddings import CohereEmbeddings
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Qdrant
@@ -35,7 +35,7 @@ def embed_pdf():
     embeddings = CohereEmbeddings(model="multilingual-22-12", cohere_api_key=cohere_api_key)
     qdrant = Qdrant.from_documents(docs, embeddings, url=qdrant_url, collection_name=collection_name, prefer_grpc=True, api_key=qdrant_api_key)
     
-    return {"collection_name":qdrant.collection_name}
+    return {"collection_name": qdrant.collection_name}
 
 # Retrieve information from a collection
 from langchain.chains.question_answering import load_qa_chain
@@ -52,7 +52,8 @@ def retrieve_info():
     embeddings = CohereEmbeddings(model="multilingual-22-12", cohere_api_key=cohere_api_key)
     qdrant = Qdrant(client=client, collection_name=collection_name, embedding_function=embeddings.embed_query)
     search_results = qdrant.similarity_search(query, k=2)
-    chain = load_qa_chain(OpenAI(openai_api_key=openai_api_key,temperature=0.2), chain_type="stuff")
+    # Specify the new model name
+    chain = load_qa_chain(OpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0.2), chain_type="stuff")
     results = chain({"input_documents": search_results, "question": query}, return_only_outputs=True)
     
-    return {"results":results["output_text"]}
+    return {"results": results["output_text"]}
